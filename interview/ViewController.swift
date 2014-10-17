e17476175450cb27862d4ad2340f21de21b2e582
Shali Nguyen <shalinguyen@gmail.com>
2014-10-16 18:09:59 -0700

    Initial commit

---
+++ interview/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  interview
+//
+//  Created by Shali Nguyen on 10/9/14.
+//  Copyright (c) 2014 yahoo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
