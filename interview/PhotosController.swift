612264e9e0ebb9f115e072299272469db06750a6
Dylan Harrington <dylanh@yahoo-inc.com>
2014-11-13 20:09:32 -0800

    Add PhotosController wip

---
+++ interview/PhotosController.swift
@@ -0,0 +1,30 @@
+//
+//  PhotosController.swift
+//  interview
+//
+//  Created by Dylan Harrington on 11/13/14.
+//  Copyright (c) 2014 yahoo. All rights reserved.
+//
+
+import UIKit
+
+class PhotosController: UIViewController {
+
+    @IBAction func onAddPhoto(sender: AnyObject) {
+        NSLog("sup")
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
+
